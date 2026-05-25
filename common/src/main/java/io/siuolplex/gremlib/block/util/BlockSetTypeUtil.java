ecfa9d8c5c74df56858c3d0ee76679f76a1eabb8
Siuolplex <lmeitrodt@gmail.com>
2026-05-24 20:35:17 -0500

    Sometimes I struggle with simple tasks like remembering to put static where it should be.
    Signed-off-by: Siuolplex <lmeitrodt@gmail.com>

--- common/src/main/java/io/siuolplex/gremlib/block/util/BlockSetTypeUtil.java
+++ common/src/main/java/io/siuolplex/gremlib/block/util/BlockSetTypeUtil.java
@@ -7,7 +7,7 @@ import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.properties.BlockSetType;
 
 public class BlockSetTypeUtil {
-    public BlockSetType of(String name,
+    public static BlockSetType of(String name,
                            boolean canOpenByHand, boolean canOpenByWindCharge, boolean canButtonBeActivatedByArrows,
                            BlockSetType.PressurePlateSensitivity pressurePlateSensitivity, SoundType soundType,
                            SoundEvent doorClose, SoundEvent doorOpen,
@@ -21,7 +21,7 @@ public class BlockSetTypeUtil {
         return BlockSetTypeMixin.registerBlockSetType(type);
     }
 
-    public BlockSetType ofWood(String name,
+    public static BlockSetType ofWood(String name,
                                SoundType soundType,
                                SoundEvent doorClose,
                                SoundEvent doorOpen,
@@ -36,35 +36,35 @@ public class BlockSetTypeUtil {
                 trapdoorClose, trapdoorOpen, pressurePlateClickOff, pressurePlateClickOn, buttonClickOff, buttonClickOn);
     }
 
-    public BlockSetType ofWood(String name) {
+    public static BlockSetType ofWood(String name) {
         return ofWood(name, SoundType.WOOD, SoundEvents.WOODEN_DOOR_CLOSE, SoundEvents.WOODEN_DOOR_OPEN,
                 SoundEvents.WOODEN_TRAPDOOR_CLOSE, SoundEvents.WOODEN_TRAPDOOR_OPEN,
                 SoundEvents.WOODEN_PRESSURE_PLATE_CLICK_OFF, SoundEvents.WOODEN_PRESSURE_PLATE_CLICK_ON,
                 SoundEvents.WOODEN_BUTTON_CLICK_OFF, SoundEvents.WOODEN_BUTTON_CLICK_ON);
     }
 
-    public BlockSetType ofNetherWood(String name) {
+    public static BlockSetType ofNetherWood(String name) {
         return ofWood(name, SoundType.NETHER_WOOD, SoundEvents.NETHER_WOOD_DOOR_CLOSE, SoundEvents.NETHER_WOOD_DOOR_OPEN,
                 SoundEvents.NETHER_WOOD_TRAPDOOR_CLOSE, SoundEvents.NETHER_WOOD_TRAPDOOR_OPEN,
                 SoundEvents.NETHER_WOOD_PRESSURE_PLATE_CLICK_OFF, SoundEvents.NETHER_WOOD_PRESSURE_PLATE_CLICK_ON,
                 SoundEvents.NETHER_WOOD_BUTTON_CLICK_OFF, SoundEvents.NETHER_WOOD_BUTTON_CLICK_ON);
     }
 
-    public BlockSetType ofBambooWood(String name) {
+    public static BlockSetType ofBambooWood(String name) {
         return ofWood(name, SoundType.BAMBOO_WOOD, SoundEvents.BAMBOO_WOOD_DOOR_CLOSE, SoundEvents.BAMBOO_WOOD_DOOR_OPEN,
                 SoundEvents.BAMBOO_WOOD_TRAPDOOR_CLOSE, SoundEvents.BAMBOO_WOOD_TRAPDOOR_OPEN,
                 SoundEvents.BAMBOO_WOOD_PRESSURE_PLATE_CLICK_OFF, SoundEvents.BAMBOO_WOOD_PRESSURE_PLATE_CLICK_ON,
                 SoundEvents.BAMBOO_WOOD_BUTTON_CLICK_OFF, SoundEvents.BAMBOO_WOOD_BUTTON_CLICK_ON);
     }
 
-    public BlockSetType ofCherryWood(String name) {
+    public static BlockSetType ofCherryWood(String name) {
         return ofWood(name, SoundType.CHERRY_WOOD, SoundEvents.CHERRY_WOOD_DOOR_CLOSE, SoundEvents.CHERRY_WOOD_DOOR_OPEN,
                 SoundEvents.CHERRY_WOOD_TRAPDOOR_CLOSE, SoundEvents.CHERRY_WOOD_TRAPDOOR_OPEN,
                 SoundEvents.CHERRY_WOOD_PRESSURE_PLATE_CLICK_OFF, SoundEvents.CHERRY_WOOD_PRESSURE_PLATE_CLICK_ON,
                 SoundEvents.CHERRY_WOOD_BUTTON_CLICK_OFF, SoundEvents.CHERRY_WOOD_BUTTON_CLICK_ON);
     }
 
-    public BlockSetType ofStone(String name) {
+    public static BlockSetType ofStone(String name) {
         return of(name,true, true, false,
                 BlockSetType.PressurePlateSensitivity.MOBS, SoundType.STONE,
                 SoundEvents.IRON_DOOR_CLOSE, SoundEvents.IRON_DOOR_OPEN,
@@ -73,7 +73,7 @@ public class BlockSetTypeUtil {
                 SoundEvents.STONE_BUTTON_CLICK_OFF, SoundEvents.STONE_BUTTON_CLICK_ON);
     }
 
-    public BlockSetType fromCopy(String name, BlockSetType baseType) {
+    public static BlockSetType fromCopy(String name, BlockSetType baseType) {
         return of(name, baseType.canOpenByHand(), baseType.canOpenByWindCharge(), baseType.canButtonBeActivatedByArrows(),
                 baseType.pressurePlateSensitivity(), baseType.soundType(), baseType.doorClose(), baseType.doorOpen(), baseType.trapdoorClose(), baseType.trapdoorOpen(),
                 baseType.pressurePlateClickOff(), baseType.pressurePlateClickOn(), baseType.buttonClickOff(), baseType.buttonClickOn()  );
